566541375e3d314f10254e1baccdce96f2fa5551
reszka303 <reszka303@op.pl>
2022-02-05 01:51:22 +0100

    2nd commit: configuration and running tests

---
+++ _01-configuration-and-running-tests/src/main/java/pl/javastart/initialconfiguration/Calculator.java
@@ -0,0 +1,8 @@
+package pl.javastart.initialconfiguration;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return 0;
+    }
+}
---
+++ _01-configuration-and-running-tests/src/test/java/pl/javastart/initialconfiguration/CalculatorTest.java
@@ -0,0 +1,14 @@
+package pl.javastart.initialconfiguration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    public void shouldTestSth() {
+
+    }
+
+}
